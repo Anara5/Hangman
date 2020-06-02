c190c732c6b4f0b7fb88ca5e93817d476fad08b4
Anara Zhunusova <55089741+Anara5@users.noreply.github.com>
2020-06-02 14:37:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React from 'react';
-import image from './images/hang.jpg'
+import React, {Component} from 'react';
 import './App.css';
+import Hangman from './Hangman';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <h1>Hänga gubbe</h1>
-        
-        <p>
-          Syftet med spelet är att gissa en slumpmässig ord<br/>
-          som valts av datorn
-        </p>
-        
-        <img src={image} className="App-logo" alt="logo" />
-
-        <h1>- - - - - -</h1>
-
-        <h1>Antal fel: 0</h1>
-
-        <button>a</button>
-    
-        <br/>
-
-        <button id="reset">Återställ</button>
-
-
-        
-      </header>
-      
+class App extends Component {
+  render(){
+    return (
+    <div className="app">
+    <Hangman/>
     </div>
   );
+  }
 }
 export default App;
-
-
