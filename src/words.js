f66a15eb0162c0fdcfbb7dc7d94259c31869a08f
Anara Zhunusova <55089741+Anara5@users.noreply.github.com>
2020-06-02 14:40:51 +0200

    Update words.js

--- src/words.js
+++ src/words.js
@@ -1,4 +1,4 @@
-var words_list = [
+const words_list = [
     "matematik",
     "ljud",
     "rullstol",
@@ -18,12 +18,14 @@ var words_list = [
     "transversal",
     "aberration",
     "agglofantisk"
-]
+];
 
-function randomWord() {
-    return words_list[Math.floor(Math.random() * words_list.length)]
-}
+const randomWord = () => {
+    return words_list[Math.floor(Math.random() * words_list.length)];
+};
 
-export {
-    randomWord
-}
+const randomWord1 = () => {
+    return words_list[Math.floor(Math.random() * words_list.length)];
+};
+
+export { randomWord, randomWord1 };
